d5fb480a9c188ebc5f6e9f106336dffe7226e714
李杰 <jezz.li@hujiang.com>
2017-09-07 09:46:11 +0800

    refactor service

---
+++ JJNetwork/Source/APIService/APIServiceDelegate.h
@@ -0,0 +1,13 @@
+//
+//  APIServiceDelegate.h
+//  JJNetwork
+//
+//  Created by Jezz on 2017/9/6.
+//  Copyright © 2017年 jezz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol APIServiceDelegate <NSObject>
+
+@end
